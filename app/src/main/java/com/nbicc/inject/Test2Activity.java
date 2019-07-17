09ed5bc942ee2c32840d769b14e909bd2704d3ec
lake <huboa1234@126.com>
2019-07-17 23:17:40 +0800

    测试广播注解

--- app/src/main/java/com/nbicc/inject/Test2Activity.java
+++ app/src/main/java/com/nbicc/inject/Test2Activity.java
@@ -3,7 +3,6 @@ package com.nbicc.inject;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.nbicc.libbindview.BindView;
 
 public class Test2Activity extends BaseActivity {
     @BindView(R.id.tv6)
